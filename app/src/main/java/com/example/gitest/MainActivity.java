30174c50858fadcf962be70c373b2dd877c37aea
Andrea Moscatelli <a.moscatelli8@gmail.com>
2020-03-31 11:54:08 +0200

    A comment from master branch

--- app/src/main/java/com/example/gitest/MainActivity.java
+++ app/src/main/java/com/example/gitest/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Main activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //a comment from brach test
+        //a comment from branch test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
